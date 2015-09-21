1381895c59d7a9475ccd9bd7b39248af7ec119c8
elex <elextraza@gmail.com>
2015-09-21 10:01:33 +0300

    Фильтр js

--- core.js
+++ core.js
@@ -24,14 +24,43 @@ function AddToCard(product_id)
 //Поиск товаров
 function Search()
 {
+
+    var sphere = '';
     $('.sphere').each(function(i,elem) {
         //console.info($(elem).text(),i);
         if ($(elem).hasClass("active"))
         {
-            console.info($(elem).html(),i);
+            sphere=sphere+"||"+$(elem).html();
         }
+
     });
 
+    var dohodnost = $(".input_slider_line").val();
+    var vlj_min = $(".input_slider_line_2").val();
+    var vlj_max = $(".input_slider_line_4").val();
+    var srok = $(".input_slider_line_3").val();
+
+    console.info(sphere,dohodnost,vlj_min,vlj_max,srok);
+
+    $.get(
+        "ajax.html",
+        {
+            //log1:1,
+            action:"Search",
+            sphere:sphere,
+            sphere:sphere,
+            sphere:sphere
+        },
+        function (data) {
+            console.info(data);
+            if(data.status=="1")
+            {
+
+                $(".ProductCountText").html(data.panel_text);
+            }
+
+        },"json"
+    ); //$.get  END
 
 
 }
